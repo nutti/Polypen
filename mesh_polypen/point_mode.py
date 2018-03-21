746f637828f9a2a47be28a1527eeeeccb116a7c5
Nutti <nutti.metro@gmail.com>
2018-03-21 09:29:12 +0900

    Refactoring

--- mesh_polypen/point_mode.py
+++ mesh_polypen/point_mode.py
@@ -3,32 +3,64 @@
 import bpy
 import bgl
 import bmesh
-from mathutils import Vector
+from mathutils import Vector, kdtree
 from bpy_extras import view3d_utils
 
 from pprint import pprint
+
+
+def get_space(context, area_type, region_type, space_type):
+
+    area = None
+    region = None
+    space = None
+
+    for area in context.screen.areas:
+        if area.type == area_type:
+            break
+    else:
+        return (None, None, None)
+    for region in area.regions:
+        if region.type == region_type:
+            break
+    for space in area.spaces:
+        if space.type == space_type:
+            break
+
+    return (area, region, space)
 
 
 class Properties:
     running = False
 
 
-class Renderer:
+class OpBase:
+    def init(self, context, event):
+        raise NotImplemented
+
+    def fini(self, context, event):
+        raise NotImplemented
+
+    def do_modal(self, context, event):
+        raise NotImplemented
+
+
+class BuildMeshOp(OpBase):
     __handle = None
 
     @staticmethod
     def handle_add(obj, context):
-        Renderer.__handle = bpy.types.SpaceView3D.draw_handler_add(
-            Renderer.draw, (obj, context), 'WINDOW', 'POST_PIXEL'
+        BuildMeshOp.__handle = bpy.types.SpaceView3D.draw_handler_add(
+            BuildMeshOp.draw, (obj, context), 'WINDOW', 'POST_PIXEL'
         )
 
     @staticmethod
     def handle_remove():
-        if Renderer.__handle is not None:
+        if BuildMeshOp.__handle is not None:
             bpy.types.SpaceView3D.draw_handler_remove(
-                Renderer.__handle, 'WINDOW'
+                BuildMeshOp.__handle, 'WINDOW'
             )
-            Renderer.__handle = None
+            BuildMeshOp.__handle = None
 
     @staticmethod
     def draw(obj, context):
@@ -38,7 +70,7 @@ class Renderer:
             return
 
         # draw point
-        bgl.glPointSize(4)
+        bgl.glPointSize(3)
         bgl.glBegin(bgl.GL_POINTS)
         bgl.glColor4f(1.0, 1.0, 1.0, 0.8)
         for p in obj.points:
@@ -85,42 +117,52 @@ class Renderer:
 
         bgl.glDisable(bgl.GL_BLEND)
 
+    def __add_mesh(self, context, region, space):
+        print("add mesh")
+        if len(self.points) < 3:
+            return
 
-def get_space(context, area_type, region_type, space_type):
+        # mouse to view3d
+        vcos = [
+            view3d_utils.region_2d_to_location_3d(
+                region, space.region_3d, p, Vector((0.0, 0.0, 0.0))
+            ) for p in self.points
+        ]
 
-    area = None
-    region = None
-    space = None
+        obj = context.active_object
+        sc = context.scene
+        bm = bmesh.from_edit_mesh(obj.data)
 
-    for area in context.screen.areas:
-        if area.type == area_type:
-            break
-    else:
-        return (None, None, None)
-    for region in area.regions:
-        if region.type == region_type:
-            break
-    for space in area.spaces:
-        if space.type == space_type:
-            break
+        verts = []
+        for vco in vcos:
+            verts.append(bm.verts.new((vco.x, vco.y, vco.z)))
 
-    return (area, region, space)
+        verts_group = []
+        idx = 2
+        while True:
+            if len(verts[idx:]) == 0:
+                break
+            group = [verts[0], verts[idx - 1]]
+            if len(verts[idx:]) <= (sc.polypen_pm_ngon - 2):
+                group.extend(verts[idx:])
+                verts_group.append(group)
+                break
+            if len(verts[idx:]) <= sc.polypen_pm_ngon:
+                group.extend(verts[idx:])
+                verts_group.append(group)
+                break
+            group.extend(verts[idx:idx + sc.polypen_pm_ngon - 2])
+            verts_group.append(group)
+            idx = idx + sc.polypen_pm_ngon - 2
+            if len(verts[idx:]) <= 0:
+                print("Unexpected error")
+                break
 
+        for g in verts_group:
+            bm.faces.new(g)
+        print(verts_group)
 
-class GeneratePolygon(bpy.types.Operator):
-
-    bl_idname = "mesh.polypen_generate_polygon"
-    bl_label = "Generate Polygon"
-    bl_description = "Generate polygon"
-    bl_option = {'REGISTER', 'UNDO'}
-
-    @classmethod
-    def poll(cls, context):
-        return not context.edit_object
-
-    def __init__(self):
-        self.points = []
-        self.stroking = False
+        bmesh.update_edit_mesh(obj.data)
 
     def __build_mesh(self, context, region, space):
         print("build mesh")
@@ -182,6 +224,204 @@ class GeneratePolygon(bpy.types.Operator):
         bmesh.update_edit_mesh(obj.data)
         bpy.ops.object.mode_set(mode='OBJECT')
 
+    def __init__(self):
+        self.points = []
+
+    def init(self, context, event):
+        mco = Vector((event.mouse_region_x, event.mouse_region_y))
+        BuildMeshOp.handle_add(self, context)
+        self.points.append(mco.copy())
+
+        return 'RUNNING_MODAL'
+
+    def fini(self, context, event):
+        area, region, space = get_space(context, 'VIEW_3D', 'WINDOW', 'VIEW_3D')
+        pprint(self.points)
+        if context.edit_object:
+            self.__add_mesh(context, region, space)
+        else:
+            self.__build_mesh(context, region, space)
+        BuildMeshOp.handle_remove()
+
+    def do_modal(self, context, event):
+        mco = Vector((event.mouse_region_x, event.mouse_region_y))
+
+        if event.type == 'LEFTMOUSE':
+            if event.value == 'PRESS':
+                self.points.append(mco.copy())
+        elif event.type == 'MOUSEMOVE':
+            self.points[-1] = mco.copy()
+        elif event.type == 'RIGHTMOUSE':
+            if event.value == 'PRESS':
+                return 'FINISHED'
+
+        return 'RUNNING_MODAL'
+
+
+class DividePolygonOp(OpBase):
+    __handle = None
+
+    @staticmethod
+    def handle_add(obj, context):
+        DividePolygonOp.__handle = bpy.types.SpaceView3D.draw_handler_add(
+            DividePolygonOp.draw, (obj, context), 'WINDOW', 'POST_PIXEL'
+        )
+
+    @staticmethod
+    def handle_remove():
+        if DividePolygonOp.__handle is not None:
+            bpy.types.SpaceView3D.draw_handler_remove(
+                DividePolygonOp.__handle, 'WINDOW'
+            )
+            DividePolygonOp.__handle = None
+
+    @staticmethod
+    def draw(obj, context):
+        pass
+
+    def __init__(self):
+        self.src_vert = None
+        self.tgt_edge = None
+        self.sel_status = {}
+
+    def __save_bmesh_status(self, bm):
+        self.sel_status["sel_verts"] = [v for v in bm.verts if v.select]
+        self.sel_status["sel_edges"] = [e for e in bm.edges if e.select]
+        self.sel_status["sel_faces"] = [f for f in bm.faces if f.select]
+
+    def __restore_bmesh_status(self, bm):
+        # clear selection
+        for v in bm.verts:
+            v.select = False
+        for e in bm.edges:
+            e.select = False
+        for f in bm.faces:
+            f.select = True
+
+        # restore selection
+        for v in self.sel_status["sel_verts"]:
+            v.select = True
+        for e in self.sel_status["sel_edges"]:
+            e.select = True
+        for f in self.sel_status["sel_faces"]:
+            f.select = True
+
+    def __get_nearest_vert(self, bm, mco, region, space):
+
+        print("save")
+        #self.__save_bmesh_status(bm)
+
+        kd = kdtree.KDTree(len(bm.verts))
+        for i, v in enumerate(bm.verts):
+            kd.insert(v.co[:], i)
+        kd.balance()
+
+        # mouse to view3d
+        mco_3d = view3d_utils.region_2d_to_location_3d(
+            region, space.region_3d, mco, Vector((0.0, 0.0, 0.0))
+        )
+
+        vco, vidx, vdist = kd.find(mco_3d)
+
+        print(vco)
+        print(vidx)
+        print(vdist)
+
+        # ret = bpy.ops.view3d.select(extend=True, location=mco)
+        # if ret == {'PASS_THROUGH'}:
+        #     return None
+
+
+        print("restore")
+        #self.__restore_bmesh_status(bm)
+
+        return bm.verts[vidx]
+
+    def __get_nearest_edge(self, bm, mco):
+        pass
+        #self.__save_bmesh_status(bm)
+
+        # ret = bpy.ops.view3d.select(extend=True, location=mco)
+        # if ret == {'PASS_THROUGH'}:
+        #     return None
+        #
+        # try:
+        #     geom = bm.select.history[-1]
+        # except IndexError:
+        #     return None
+
+        #self.__restore_bmesh_status(bm)
+
+        #if isinstance(geom, bmesh.types.BMVert):
+        #    return geom
+
+    def init(self, context, event):
+        if not context.edit_object:
+            return 'CANCELLED'
+
+        mco = Vector((event.mouse_region_x, event.mouse_region_y))
+        area, region, space = get_space(context, 'VIEW_3D', 'WINDOW', 'VIEW_3D')
+
+        obj = context.active_object
+        bm = bmesh.from_edit_mesh(obj.data)
+
+        print("__get_nearest_vert")
+
+        nv = self.__get_nearest_vert(bm, mco, region, space)
+        if nv:
+            self.src_vert = nv
+
+        BuildMeshOp.handle_add(self, context)
+
+        return 'RUNNING_MODAL'
+
+    def fini(self, context, event):
+        if not context.edit_object:
+            return
+
+        if not self.src_vert:
+            return
+        mco = Vector((event.mouse_region_x, event.mouse_region_y))
+
+        obj = context.active_object
+        bm = bmesh.from_edit_mesh(obj.data)
+
+        ne = self.__get_nearest_edge(bm, mco)
+        if ne:
+            self.src_vert = ne
+
+        BuildMeshOp.handle_remove()
+
+    def do_modal(self, context, event):
+        if context.edit_object:
+            return 'CANCELLED'
+
+        if event.type == 'LEFTMOUSE':
+            if event.value == 'PRESS':
+                return 'FINISHED'
+
+        return 'RUNNING_MODAL'
+
+        # mco = Vector((event.mouse_region_x, event.mouse_region_y))
+        #
+        # if event.type == 'LEFTMOUSE':
+        #     if event.value == 'PRESS':
+        #         self.points.append(mco.copy())
+        # elif event.type == 'MOUSEMOVE':
+        #     self.points[-1] = mco.copy()
+
+
+class GeneratePolygon(bpy.types.Operator):
+
+    bl_idname = "mesh.polypen_generate_polygon"
+    bl_label = "Generate Polygon"
+    bl_description = "Generate polygon"
+    bl_option = {'REGISTER', 'UNDO'}
+
+    def __init__(self):
+        self.stroking = False
+        self.op = None
+
     def modal(self, context, event):
         props = context.scene.polypen_props.point_mode
 
@@ -202,25 +442,25 @@ class GeneratePolygon(bpy.types.Operator):
             if event.value == 'PRESS':
                 # start stroking
                 if not self.stroking:
-                    self.points = []
-                    Renderer.handle_add(self, context)
+                    if event.shift:
+                        print("DividePolygon")
+                        self.op = DividePolygonOp()
+                    else:
+                        print("BuildMesh")
+                        self.op = BuildMeshOp()
+                    ret = self.op.init(context, event)
+                    # [TODO] ret
                     self.stroking = True
                     print("stroking started")
-                self.points.append(mco.copy())
-                return {'RUNNING_MODAL'}
-        elif event.type == 'RIGHTMOUSE':
-            if event.value == 'PRESS':
-                # end stroking
+
+        if self.op:
+            ret = self.op.do_modal(context, event)
+            if ret != 'RUNNING_MODAL':
                 if self.stroking:
-                    pprint(self.points)
-                    self.__build_mesh(context, region, space)
-                    Renderer.handle_remove()
+                    self.op.fini(context, event)
+                    self.op = None
                     self.stroking = False
-                    print("stroking ended")
-                return {'RUNNING_MODAL'}
-        elif event.type == 'MOUSEMOVE':
-            if self.stroking:
-                self.points[-1] = mco.copy()
+                return {'FINISHED'}
 
         if context.area:
             context.area.tag_redraw()
--- mesh_polypen/view3d_polypen.py
+++ mesh_polypen/view3d_polypen.py
@@ -27,3 +27,26 @@ class OBJECT_PT_PolyPen(bpy.types.Panel):
                             text="End", icon='PAUSE')
 
         layout.prop(sc, "polypen_pm_ngon")
+
+class EDIT_PT_PolyPen(bpy.types.Panel):
+
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+    bl_label = "Polypen"
+    bl_category = "Polypen"
+    bl_context = 'mesh_edit'
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw(self, context):
+        layout = self.layout
+        props = context.scene.polypen_props
+        sc = context.scene
+
+        if not props.point_mode.running:
+            layout.operator(point_mode.GeneratePolygon.bl_idname,
+                            text="Start", icon='PLAY')
+        else:
+            layout.operator(point_mode.GeneratePolygon.bl_idname,
+                            text="End", icon='PAUSE')
+
+        layout.prop(sc, "polypen_pm_ngon")
